6b4606966b4d2370fad4df2bacf7b65b9da2919c
Anton Agafonnikov <feograf@mail.ru>
2023-10-01 13:34:00 +0300

    Added Pre/PostAuthorize and query param on endpoint "/personal-greeting"

--- src/main/java/ru/netology/hw_rest_authorization/controller/AuthorizationController.java
+++ src/main/java/ru/netology/hw_rest_authorization/controller/AuthorizationController.java
@@ -1,15 +1,12 @@
 package ru.netology.hw_rest_authorization.controller;
 
-import jakarta.annotation.security.RolesAllowed;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.netology.hw_rest_authorization.security.WebSecurityConfig;
-import ru.netology.hw_rest_authorization.service.Authorities;
 import ru.netology.hw_rest_authorization.service.AuthorizationService;
 
 @RestController
@@ -20,52 +17,76 @@ public class AuthorizationController {
         this.service = service;
     }
 
+    //Endpoint достпен всем
     @GetMapping("/home")
     public String welcomeToHome() {
         return "Welcome to HOME =)";
     }
 
+    //Endpoint доступен только авторизованным пользователям с ролью WRITE:
+    // ИМЯ  ПАРОЛЬ
+    // user password
+    // ben 1234
     @GetMapping("/create-post")
     public String createPost() {
         return "Welcome, writer!";
     }
 
+    //Endpoint доступен только авторизованным пользователям с ролью READ:
+    // ИМЯ  ПАРОЛЬ
+    // user password
+    // sam  qwerty
     @GetMapping("/read-post")
     public String readPost() {
         return "Welcome, user! (There is a very interesting text here)";
     }
+
+    //Endpoint доступен только авторизованным пользователям с любой из перечисленных ролей: WRITE/READ/DELETE
+    // ИМЯ  ПАРОЛЬ
+    // user password
+    // ben 1234
+    // sam  qwerty
     @GetMapping("/contacts")
+    @PreAuthorize("hasAnyRole('WRITE', 'READ', 'DELETE')")
     public String openContact() {
         return "+7-123-543-44-66    Moscow, Dostoevskay st, 12";
     }
 
+    //Endpoint доступен только авторизованным пользователям с любой из перечисленных ролей: WRITE/DELETE
+    // ИМЯ  ПАРОЛЬ
+    // user password
+    // ben 1234
+    //-----
+    // Если сюда попробует зайти пользователь sam (qwerty), то код метода выполнится, в консоль выведется "10",
+    // а в ответ на запрос вернётся - 403
     @GetMapping("/data-base-users")
+    @PostAuthorize("hasAnyRole('WRITE', 'DELETE')")
     public String getDataBaseUsers() {
+        int methodEntryDemonstration = 5;
+        methodEntryDemonstration += 5;
+        System.out.println(methodEntryDemonstration);
         return """
-                1   Petya   pet@mail.ru
-                
-                2   Leha   leha@mail.ru
-                
+                1   Petya   pet@mail.ru   
+                2   Leha   leha@mail.ru 
                 3   Vasya   vasya@mail.ru
                 """;
     }
 
+    //Endpoint доступен только авторизованным пользователям с любой из перечисленных ролей: WRITE/READ/DELETE
+    // ИМЯ  ПАРОЛЬ
+    // user password
+    // ben 1234
+    // sam  qwerty
+    //-----
+    // Если в query-параметре передать имя, отличное от авторизованного пользователя, то всплывёт предупреждение,
+    // иначе - вас персонально попреветствуют
     @GetMapping("/personal-greeting")
-    @PreAuthorize("#username = authentication.principal.username")
-    public String getPersonalGreeting(String username) {
-        return username;
+    public String getPersonalGreeting(@RequestParam String queryUser,
+            @AuthenticationPrincipal User user) {
+        String authorizedUser = user.getUsername();
+        return queryUser.equals(authorizedUser)?
+                ("Hello " + authorizedUser)
+                :"WARNING! Another user is currently logged in";
     }
-
 }
-
-/*
-- один из методов возвращает значения только для пользователей с ролью "READ" (используйте @Secured);
-- один из методов возвращает значения только для пользователей с ролью "WRITE" (используйте @RolesAllowed);
-- один из методов возвращает значения, если у пользователя есть хотя бы одна из ролей из "WRITE", "DELETE"
-    (используйте pre/post аннотации);
-- один из методов, который принимает в качестве query-параметра имя пользователя (username), должен
-    возвращает значения, только если у пользователя username совпадает с именем пользователя в вашем объекте
-    Authentication, который Spring security сохраняет в SecurityContextHolder после успешной аутентификации.
- */
-
 
--- src/main/java/ru/netology/hw_rest_authorization/security/WebSecurityConfig.java
+++ src/main/java/ru/netology/hw_rest_authorization/security/WebSecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -31,14 +33,14 @@ public class WebSecurityConfig {
                         .requestMatchers("/read-post")
                         .hasRole("READ")
                 )
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/contact")
-                        .hasAnyRole("WRITE", "READ", "DELETE")
-                )
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/data-base-users") //TODO POST???
-                        .hasAnyRole("WRITE", "DELETE")
-                )
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/contact")
+//                        .hasAnyRole("WRITE", "READ", "DELETE")
+//                )
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/data-base-users")
+//                        .hasAnyRole("WRITE", "DELETE")
+//                )
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/personal-greeting")
                         .hasAnyRole("WRITE", "READ", "DELETE")
